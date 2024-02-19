0cd09f66da1ea1065a2ef70c01862331230c360b
Benjamin Reece <b@bnjmn.org>
2024-02-19 11:13:22 -0700

    sentry setup

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,20 @@
-import { RemixBrowser } from '@remix-run/react'
-import { startTransition } from 'react'
+import * as Sentry from "@sentry/remix";
+import { RemixBrowser, useLocation, useMatches } from '@remix-run/react';
+import { startTransition, useEffect } from 'react';
 import { hydrateRoot } from 'react-dom/client'
+
+Sentry.init({
+    dsn: "https://8971f9dac208a0d92c5538a83935b7b8@o4506774794731520.ingest.sentry.io/4506774813540352",
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+
+    integrations: [Sentry.browserTracingIntegration({
+      useEffect,
+      useLocation,
+      useMatches
+    }), Sentry.replayIntegration()]
+})
 
 if (ENV.MODE === 'production' && ENV.SENTRY_DSN) {
 	import('./utils/monitoring.client.tsx').then(({ init }) => init())
